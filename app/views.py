187d7e22cb889780123644492e55312ae24fc8e4
CoderTrudy <raeann.clarke@mymona.uwi.edu>
2024-04-09 13:11:01 -0500

    created migrations folder,movies table and falsk api

--- app/views.py
+++ app/views.py
@@ -8,7 +8,10 @@ This file creates your application.
 from app import app
 from flask import render_template, request, jsonify, send_file
 import os
-
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+from forms import MovieForm, form_errors
+from models import db, Movie
 
 ###
 # Routing for your application.
@@ -17,6 +20,20 @@ import os
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
+
+
+@app.route('/submit_movie', methods=['GET', 'POST'])
+def submit_movie():
+    form = MovieForm()
+    if form.validate_on_submit():
+        # Process the form data
+        title = form.title.data
+        description = form.description.data
+        poster = form.poster.data
+        # Save the movie data, e.g., to a database
+        return 'Movie submitted successfully!'
+    return render_template('submit_movie.html', form=form)
+
 
 
 ###
@@ -37,6 +54,44 @@ def form_errors(form):
             error_messages.append(message)
 
     return error_messages
+
+
+# Function to save uploaded file
+def save_file(file):
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return filename
+
+# Route to add movies
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    form = MovieForm(request.form)
+    if form.validate():
+        title = form.title.data
+        description = form.description.data
+        poster = request.files['poster']
+        poster_filename = save_file(poster)
+        
+        # Create new movie object
+        new_movie = Movie(title=title, description=description, poster=poster_filename)
+        
+        # Save the movie to the database
+        db.session.add(new_movie)
+        db.session.commit()
+        
+        # Return JSON response with movie details
+        return jsonify({
+            "message": "Movie Successfully added",
+            "title": new_movie.title,
+            "poster": new_movie.poster,
+            "description": new_movie.description
+        }), 200
+    else:
+        # Return JSON response with form errors
+        errors = form_errors(form)
+        return jsonify({"errors": errors}), 400
+
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
@@ -61,3 +116,6 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
